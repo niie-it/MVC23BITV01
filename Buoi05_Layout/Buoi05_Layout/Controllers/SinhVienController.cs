b5b0a433d3ebd55093579d1cf6e48690dec35bd6
HIENLTH <hyhien@gmail.com>
2026-07-03 09:04:20 +0700

    jQuery Validation

---
+++ Buoi05_Layout/Buoi05_Layout/Controllers/SinhVienController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi05_Layout.Controllers
+{
+    public class SinhVienController : Controller
+    {
+        public IActionResult KiemTraMaSV(string MaSV)
+        {
+            if(new[] { "admin", "employee" }.Contains(MaSV))
+            {
+                return Json($"Mã sinh viên {MaSV} không hợp lệ.");
+            }
+            else
+            {
+                return Json(true);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult DangKy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DangKy(string hoTen)
+        {
+            return View("DangKyThanhCong");
+        }
+    }
+}
---
+++ Buoi05_Layout/Buoi05_Layout/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Buoi05_Layout.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
