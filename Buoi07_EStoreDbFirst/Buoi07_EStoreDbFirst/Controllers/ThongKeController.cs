4bdfb99b31b94ff30a43de6cf7b388b21950e1ad
HIENLTH <hyhien@gmail.com>
2026-07-17 11:59:35 +0700

    Thong ke

---
+++ Buoi07_EStoreDbFirst/Buoi07_EStoreDbFirst/Controllers/ThongKeController.cs
@@ -0,0 +1,35 @@
+﻿using Buoi07_EStoreDbFirst.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi07_EStoreDbFirst.Controllers
+{
+    public class ThongKeController : Controller
+    {
+        private readonly MyeStoreContext _context;
+
+        public ThongKeController(MyeStoreContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ThongKeTheoLoai()
+        {
+            var data = _context.ChiTietHds
+                .GroupBy(cthd => new
+                {
+                    TenLoai = cthd.MaHhNavigation.MaLoaiNavigation.TenLoai,
+                    MaLoai = cthd.MaHhNavigation.MaLoai
+
+                })
+                .Select(g => new
+                {
+                    TenLoai = g.Key.TenLoai,
+                    MaLoai = g.Key.MaLoai,
+                    TongSoLuong = g.Sum(cthd => cthd.SoLuong),
+                    TongDoanhThu = g.Sum(cthd => cthd.SoLuong * cthd.DonGia)
+                })
+                .ToList();
+            return Json(data);
+        }
+    }
+}
