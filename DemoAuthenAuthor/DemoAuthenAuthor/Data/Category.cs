10c7f6e711f409020dbc4f6d8bad2ede3cd307b9
HIENLTH <hyhien@gmail.com>
2026-07-31 11:16:46 +0700

    Demo Authen

---
+++ DemoAuthenAuthor/DemoAuthenAuthor/Data/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoAuthenAuthor.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int? CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category? Category { get; set; }
+    }
+}
